97272ee5d74d34e372ef6ccd9bc64d2d504271be
javiber <javier.berneche@gmail.com>
2020-01-13 15:05:28 -0300

    Added mirror option to dual monitor script

--- dual_monitor.sh
+++ dual_monitor.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-PRIMARY='eDP-1'
+PRIMARY='eDP1'
 MONITOR=$(xrandr | grep -v disconnected | grep connected | grep -v $PRIMARY | cut -d' ' -f1)
+MIRROR=0
+
+while [[ $# -gt 0 ]];
+do
+   echo $1
+   case $1 in
+    	-m|--mirror)
+    	MIRROR=1
+    	shift # past argument
+	shift
+    	;;
+   esac
+done
 
 if [ -z "$MONITOR" ]; then
 	echo "turning off monitors"
@@ -11,11 +24,14 @@ if [ -z "$MONITOR" ]; then
 	done
 else
 	echo "configuring $MONITOR"
-	xrandr --output $MONITOR --auto --above $PRIMARY
-	i3-msg workspace 10, move workspace to output $MONITOR
-	for i in $(seq 1 9); do 
-		i3-msg workspace $i, move workspace to $PRIMARY; 
-	done
-	i3-msg workspace 10
-
+	if [ $MIRROR -eq 1 ]; then
+		xrandr --output $MONITOR --auto --same-as $PRIMARY
+	else
+		xrandr --output $MONITOR --auto --above $PRIMARY
+		i3-msg workspace 10, move workspace to output $MONITOR
+		for i in $(seq 1 9); do 
+			i3-msg workspace $i, move workspace to $PRIMARY; 
+		done
+		i3-msg workspace 10
+	fi
 fi
